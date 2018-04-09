561be4e51577da51a8ae1b5bb128002d102f5ab4
PhilippPro <philipp_probst@gmx.de>
2018-04-09 11:11:47 +0200

    add some lines for the first learner

--- R/autoMlr.R
+++ R/autoMlr.R
@@ -1,6 +1,14 @@
-autoMlr = function(algorithms, task, runtime) {
+autoMlr = function(task, runtime) { 
+  # add algorithms as input
   
-  
+  library(mlr)
+  type = getTaskType(task)
+  lrn.name = paste0(type, ".glmnet")
+  lrn = makeLearner(cl = lrn.name, par.vals = as.list(defaults$glmnet$default[1,]))
+  mod = train(lrn, task)
+
   # autoxgboost
   # tuneRanger
+  
+  return(mod)
 }
