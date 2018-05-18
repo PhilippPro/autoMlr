3d38207b0de2cfb68fb764f13ab502646590325f
PhilippPro <philipp_probst@gmx.de>
2018-05-18 15:58:44 +0200

    mini-update

--- R/autoMlr.R
+++ R/autoMlr.R
@@ -17,23 +17,31 @@ autoMlr = function(task, runtime) {
     
   library(mlr)
   type = getTaskType(task)
+  lrn = list()
   #lrn.name = ifelse(type == "regr", "regr.lm", "classif.logreg")
   #lrn0 = makeLearner(cl = lrn.name, predict.type = "prob")
   lrn.name = paste0(type, ".glmnet")
-  lrn1 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$glmnet$default[1,]), predict.type = "prob")
+  lrn[[1]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$glmnet$default[1,]), predict.type = "prob")
   lrn.name = paste0(type, ".rpart")
-  lrn2 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$rpart$default[1,]), predict.type = "prob")
+  lrn[[2]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$rpart$default[1,]), predict.type = "prob")
   lrn.name = paste0(type, ".kknn")
-  lrn3 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$kknn$default[1,, drop = F]), predict.type = "prob")
+  lrn[[3]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$kknn$default[1,, drop = F]), predict.type = "prob")
   lrn.name = paste0(type, ".svm")
-  lrn4 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$svm$default[1,][,!is.na(defaults$svm$default[1,])]), predict.type = "prob")
+  lrn[[4]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$svm$default[1,][,!is.na(defaults$svm$default[1,])]), predict.type = "prob")
   lrn.name = paste0(type, ".ranger")
-  lrn5 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$ranger$default[1,]), predict.type = "prob")
+  lrn[[5]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$ranger$default[1,]), predict.type = "prob")
   lrn.name = paste0(type, ".xgboost")
-  lrn6 = makeLearner(cl = lrn.name, par.vals = as.list(defaults$xgboost$default[1,]), predict.type = "prob")
+  lrn[[6]] = makeLearner(cl = lrn.name, par.vals = as.list(defaults$xgboost$default[1,]), predict.type = "prob")
 
   # makeStackedLearner not modular enough. Change this!
   lrn = makeStackedLearner(base.learners = list(lrn1, lrn2, lrn3, lrn4, lrn5, lrn6), predict.type = "prob", method = "hill.climb")
+  
+  #mod = list()
+  #for(i in seq_along(lrn)) { 
+  #  print(paste("Train", lrn[[i]]$short.name, "model"))
+  #  mod[[i]] = train(lrn[[i]], task)
+  #}
+  
   mod = train(lrn, task)
   
   # Zeitbeschränkung:
@@ -49,5 +57,7 @@ autoMlr = function(task, runtime) {
   
   return(mod)
 }
+
+
 
 
